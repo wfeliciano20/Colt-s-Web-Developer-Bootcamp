4a0d875c3ed6f9c2e8c361dc745e9552d506e6e8
william feliciano <williamfeliciano@williams-MacBook-Pro.local>
2020-04-01 21:58:20 -0400

    refactoring todo app

--- Colt's-Web-Developer-Bootcamp/Javascript/Arrays - SimpleTodoList/app.js
+++ Colt's-Web-Developer-Bootcamp/Javascript/Arrays - SimpleTodoList/app.js
@@ -22,13 +22,36 @@ var input = prompt("What would you like to do?");
 
 while (input !== 'quit') {
     if (input === 'list') {
-        console.log(todos);
+        listTodos();
     } else if (input === 'new') {
-        const newTodo = prompt('Enter new todo');
-        todos.push(newTodo);
+        addTodo();
+    } else if (input === 'delete') {
+        deleteTodo();
     }
 
     input = prompt("What would you like to do?");
 }
 
 console.log("Ok, you quit the app");
+
+const listTodos = () => {
+    console.log('************');
+    let index = 0;
+    for (const todo of todos) {
+        console.log(index + ": " + todo);
+        index++;
+    }
+    console.log('************');
+};
+
+const addTodo = () => {
+    const newTodo = prompt('Enter new todo');
+    todos.push(newTodo);
+    console.log('Added todo');
+};
+
+const deleteTodo = () => {
+    let indexToDelete = prompt("Enter index of todo to delete");
+    todos.splice(indexToDelete, 1);
+    console.log('Deleted todo');
+};
