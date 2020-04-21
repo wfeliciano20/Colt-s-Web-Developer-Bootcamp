c4d863f0a251bfca2fd420524a05afad80bdddee
william feliciano <williamfeliciano@williams-MacBook-Pro.local>
2020-04-20 23:56:02 -0400

    nodemon files

--- Colt's-Web-Developer-Bootcamp/IntroToExpress/app.js
+++ Colt's-Web-Developer-Bootcamp/IntroToExpress/app.js
@@ -9,6 +9,15 @@ app.get('/bye', function(req, res) {
     res.send('Goodbye');
 });
 
+app.get('/r/:subredditName', function(req, res) {
+    const title = req.params.subredditName;
+    res.send('Welcome to the ' + title + ' page');
+});
+
+app.get('*', function(requ, res) {
+    res.send('You are a star!');
+});
+
 app.listen(3000, function() {
     console.log('Server listening on port 3000');
 });
