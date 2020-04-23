4e6814229d723f044ebcc6d6408e614207731c4c
william feliciano <williamfeliciano@williams-MacBook-Pro.local>
2020-04-23 19:42:19 -0400

    movie_Search_app initial commit

--- Colt's-Web-Developer-Bootcamp/APIS/app.js
+++ Colt's-Web-Developer-Bootcamp/APIS/app.js
@@ -3,7 +3,7 @@ const request = require('request');
 request('https://jsonplaceholder.typicode.com/users/1', function(error, response, body) {
 
     if (!error && response.statusCode == 200) {
-        let parsedData = JSON.parse(body);
-        console.log(parsedData['query']['results']['channel']['astronomy']['sunset']);
+        const parsedData = JSON.parse(body);
+        console.log(parsedData.name + ' lives in ' + parsedData.address.city);
     }
 });
