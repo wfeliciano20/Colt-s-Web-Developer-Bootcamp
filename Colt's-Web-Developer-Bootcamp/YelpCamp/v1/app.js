07fa2f1e949260d25a0ddce2382270849ceec14f
william feliciano <williamfeliciano@williams-MacBook-Pro.local>
2020-04-25 19:58:44 -0400

    header footer landing campgrounds ejs files and body parser and update to app.js

--- Colt's-Web-Developer-Bootcamp/YelpCamp/v1/app.js
+++ Colt's-Web-Developer-Bootcamp/YelpCamp/v1/app.js
@@ -1,32 +1,40 @@
-const express = require('express');
+const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
 
-app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
 
-
-app.get('/', function(req, res) {
-    res.render('landing');
+app.get("/", function(req, res) {
+    res.render("landing");
 });
 
-
-app.get('/campgrounds', function(req, res) {
+app.get("/campgrounds", function(req, res) {
     const campgrounds = [{
-        name: "Salmon Creek",
-        image: "https://api.creativecommons.engineering/t/600/https://farm9.staticflickr.com/8745/28660881602_2abf1312ca_m.jpg"
-    }, {
-        name: "Granite Hill",
-        image: "https://api.creativecommons.engineering/t/600/https://live.staticflickr.com/8003/28660877652_56f7af6fc1_m.jpg"
-    }, {
-        name: "Mountain Goat's Rest",
-        image: "https://api.creativecommons.engineering/t/600/https://live.staticflickr.com/8763/28148495284_679e52bc4f_m.jpg"
-    }]
+            name: "Salmon Creek",
+            image: "https://api.creativecommons.engineering/t/600/https://live.staticflickr.com/3336/3551756947_7799d72c13_m.jpg",
+        },
+        {
+            name: "Granite Hill",
+            image: "https://api.creativecommons.engineering/t/600/https://farm9.staticflickr.com/8003/28660877652_56f7af6fc1_m.jpg",
+        },
+        {
+            name: "Mountain Goat's Rest",
+            image: "https://api.creativecommons.engineering/t/600/https://live.staticflickr.com/8763/28148495284_679e52bc4f_m.jpg",
+        },
+    ];
 
-    res.render('campgrounds', {
-        campgrounds: campgrounds
+    res.render("campgrounds", {
+        campgrounds: campgrounds,
     });
 });
 
+app.post("/campgrounds", function(req, res) {
+    //get data from form and add to campgrounds array
+    res.send("You hit the post route");
+    //redirect back to campgrounds page
+});
 
 app.listen(3000, function() {
-    console.log('The YelpCamp server has started!');
-})
+    console.log("The YelpCamp server has started!");
+});
