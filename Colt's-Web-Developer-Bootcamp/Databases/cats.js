87f6ead2518954a09c11e2fdeda652c4d818e4e2
william feliciano <williamfeliciano@williams-MacBook-Pro.local>
2020-04-28 19:17:12 -0400

    final chanages

--- Colt's-Web-Developer-Bootcamp/Databases/cats.js
+++ Colt's-Web-Developer-Bootcamp/Databases/cats.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useUnifiedTopology', true);
 mongoose.connect("mongodb://localhost/cat_app");
 
 const catSchema = new mongoose.Schema({
@@ -15,14 +17,43 @@ let george = new Cat({
     temperament: 'Grouchy'
 });
 
-george.save(function(err, cat) {
-    if (err) {
-        console.log('Something went wrong.')
-    } else {
-        console.log('We just save a cat to the DB');
-        console.log(cat);
-    }
+let mrsNorris = new Cat({
+    name: "Mrs. Norris",
+    age: 7,
+    temperament: "Evil"
 });
+
 //adding a new cat to the DB
 
+// mrsNorris.save(function(err, cat) {
+//     if (err) {
+//         console.log('Something went wrong.')
+//     } else {
+//         console.log('We just save a cat to the DB');
+//         console.log(cat);
+//     }
+// });
+
+// using create method
+// Cat.create({
+//     name: "Snow White",
+//     age: 15,
+//     temperament: 'Bland'
+// }, function(err, cat) {
+//     if (err) {
+//         console.log(err);
+//     } else {
+//         console.log(cat);
+//     }
+// })
+
 //retrieve all cats from the DB and console.log each one
+
+Cat.find({}, function(err, cats) {
+    if (err) {
+        console.log("oh no, error")
+    } else {
+        console.log('ALL THE CATS.....');
+        console.log(cats);
+    }
+});
