87c2372fbb67bdc8b05014daea88de704cd8d403
william feliciano <williamfeliciano@williams-MacBook-Pro.local>
2020-04-30 15:40:25 -0400

    updated files to add description field to campgrounds

--- Colt's-Web-Developer-Bootcamp/YelpCamp/v2/app.js
+++ Colt's-Web-Developer-Bootcamp/YelpCamp/v2/app.js
@@ -14,7 +14,8 @@ app.set("view engine", "ejs");
 //Schema setup
 const campgroundsSchema = new mongoose.Schema({
     name: String,
-    image: String
+    image: String,
+    description: String
 });
 
 const Campground = mongoose.model("Campground", campgroundsSchema);
@@ -23,6 +24,7 @@ app.get("/", function(req, res) {
     res.render("landing");
 });
 
+//Index - Show all campgrounds
 app.get("/campgrounds", function(req, res) {
     Campground.find({}, function(err, allCampgrounds) {
         if (err) {
@@ -36,17 +38,16 @@ app.get("/campgrounds", function(req, res) {
 
 });
 
-app.get("/campgrounds/new", function(req, res) {
-    res.render("new.ejs");
-});
-
+//CREATE - add new campground to DB
 app.post("/campgrounds", function(req, res) {
     //get data from form and add to campgrounds array
     const name = req.body.name;
     const image = req.body.image;
+    const description = req.body.description;
     const newCampground = {
         name: name,
-        image: image
+        image: image,
+        description: description
     };
     Campground.create(newCampground, function(err, newlyCreated) {
         if (err) {
@@ -56,9 +57,21 @@ app.post("/campgrounds", function(req, res) {
             res.redirect("/campgrounds");
         }
     });
-
 });
 
+//NEW - show form to create new campground
+app.get("/campgrounds/new", function(req, res) {
+    res.render("new.ejs");
+});
+
+//SHOW -  Shows info about one campground
+app.get('/campgrounds/:id', function(req, res) {
+    //find the campground with provided ID
+    //render show template with that campground
+    res.send('This WILL BE THE SHOW PAGE ONE DAY!');
+});
+
+// Set port 3000 to listen to requests
 app.listen(3000, function() {
     console.log("The YelpCamp server has started!");
 });
