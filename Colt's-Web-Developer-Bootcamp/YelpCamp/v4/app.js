58b93c12a48fe4c1ef01aaca16c8122e97127fa6
william feliciano <williamfeliciano@williams-MacBook-Pro.local>
2020-05-05 19:51:25 -0400

    v4 initial files

---
+++ Colt's-Web-Developer-Bootcamp/YelpCamp/v4/app.js
@@ -0,0 +1,80 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const mongoose = require('mongoose');
+const Campground = require("./models/campground");
+const seedDB = require("./seeds");
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useUnifiedTopology', true);
+mongoose.connect("mongodb://localhost/yelp_camp");
+
+seedDB();
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.set("view engine", "ejs");
+
+
+app.get("/", function(req, res) {
+    res.render("landing");
+});
+
+//Index - Show all campgrounds
+app.get("/campgrounds", function(req, res) {
+    Campground.find({}, function(err, allCampgrounds) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render("index", {
+                campgrounds: allCampgrounds,
+            });
+        }
+    });
+
+});
+
+//CREATE - add new campground to DB
+app.post("/campgrounds", function(req, res) {
+    //get data from form and add to campgrounds array
+    const name = req.body.name;
+    const image = req.body.image;
+    const description = req.body.description;
+    const newCampground = {
+        name: name,
+        image: image,
+        description: description
+    };
+    Campground.create(newCampground, function(err, newlyCreated) {
+        if (err) {
+            console.log(err);
+        } else {
+            //redirect back to campgrounds page
+            res.redirect("/campgrounds");
+        }
+    });
+});
+
+//NEW - show form to create new campground
+app.get("/campgrounds/new", function(req, res) {
+    res.render("new.ejs");
+});
+
+//SHOW -  Shows info about one campground
+app.get('/campgrounds/:id', function(req, res) {
+    //find the campground with provided ID
+    Campground.findById(req.params.id).populate("comments").exec(function(err, foundCampground) {
+        if (err) {
+            console.log(err);
+        } else {
+            //render show template with that campground
+            res.render('show', {
+                campground: foundCampground
+            });
+        }
+    });
+});
+
+// Set port 3000 to listen to requests
+app.listen(3000, function() {
+    console.log("The YelpCamp server has started!");
+});
