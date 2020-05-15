13f0416aeb9e61ebb535ae8898ebdaf267c16151
william feliciano <williamfeliciano@williams-MacBook-Pro.local>
2020-05-14 21:17:11 -0400

    app and user js files updated

--- Colt's-Web-Developer-Bootcamp/Authentication/app.js
+++ Colt's-Web-Developer-Bootcamp/Authentication/app.js
@@ -1,10 +1,34 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
+const passport = require("passport");
+const localStrategy = require("passport-local");
+const passportLocalMongoose = require("passport-local-mongoose");
+const User = require("./models/user");
 const mongoose = require("mongoose");
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useUnifiedTopology', true);
 mongoose.connect("mongodb://localhost/auth_demo_app");
 
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
+app.use(require("express-session")({
+    secret: "Rusty is the best and cutest dog in the world",
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+//===============================
+// Routes
+//===============================
 app.get("/", function(req, res) {
     res.render("home");
 });
@@ -12,6 +36,7 @@ app.get("/", function(req, res) {
 app.get("/secret", function(req, res) {
     res.render("secret");
 });
+
 
 
 app.listen(3000, function() {
