ef2ca33919d0c7c41ad267d55be23914950739ba
william feliciano <williamfeliciano@williams-MacBook-Pro.local>
2020-05-21 20:49:23 -0400

    comment update and destroy

--- Colt's-Web-Developer-Bootcamp/YelpCamp/v10/routes/comments.js
+++ Colt's-Web-Developer-Bootcamp/YelpCamp/v10/routes/comments.js
@@ -48,6 +48,47 @@ router.post("/", isLoggedIn, function(req, res) {
     });
 });
 
+//===============================================
+//      Comments edit Routes
+//===============================================
+//edit a comment
+router.get("/:comment_id/edit", checkCommentOwnership, function(req, res) {
+    Comment.findById(req.params.comment_id, function(err, foundComment) {
+        if (err) {
+            res.redirect("back");
+        } else {
+            res.render("comments/edit", {
+                campground_id: req.params.id,
+                comment: foundComment
+            });
+        }
+    });
+});
+
+// COMMENT UPDATE
+router.put("/:comment_id", checkCommentOwnership, function(req, res) {
+    Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, function(err, updatedComment) {
+        if (err) {
+            res.redirect("back");
+        } else {
+            res.redirect("/campgrounds/" + req.params.id);
+        }
+    });
+});
+
+// COMMENT DESTROY ROUTE
+router.delete("/:comment_id", checkCommentOwnership, function(req, res) {
+    //findByIdAndRemove
+    Comment.findByIdAndRemove(req.params.comment_id, function(err) {
+        if (err) {
+            res.redirect("back");
+        } else {
+            res.redirect("/campgrounds/" + req.params.id);
+        }
+    });
+});
+
+
 
 //===============================================
 //  Middleware isLoggedIn
@@ -57,6 +98,26 @@ function isLoggedIn(req, res, next) {
         return next();
     }
     res.redirect("/login")
+}
+
+
+function checkCommentOwnership(req, res, next) {
+    if (req.isAuthenticated()) {
+        Comment.findById(req.params.comment_id, function(err, foundComment) {
+            if (err) {
+                res.redirect("back");
+            } else {
+                // does user own the comment?
+                if (foundComment.author.id.equals(req.user._id)) {
+                    next();
+                } else {
+                    res.redirect("back");
+                }
+            }
+        });
+    } else {
+        res.redirect("back");
+    }
 }
 
 
