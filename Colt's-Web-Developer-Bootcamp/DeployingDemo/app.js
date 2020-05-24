2c32da0796e26adf47dcec47e1ae8943061d6c9b
william feliciano <williamfeliciano@williams-MacBook-Pro.local>
2020-05-24 17:49:26 -0400

    routes and templates setup

---
+++ Colt's-Web-Developer-Bootcamp/DeployingDemo/app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+
+app.set("view engine", "ejs");
+
+app.get("/", function(req, res) {
+    res.render("home");
+});
+
+app.get("/about", function(req, res) {
+    res.render("about");
+})
+
+app.listen(3000, function() {
+    console.log("server has started");
+});
